26ab9dd62b4d1dcac82b2e871e5a39fa65c8ca65
Clear Code <62236986+clear-code-projects@users.noreply.github.com>
2024-04-14 15:47:21 +0100

    Add files via upload

---
+++ data/tilesets/indoor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="indoor" tilewidth="64" tileheight="64" tilecount="90" columns="10">
+ <image source="../../graphics/tilesets/indoor.png" width="640" height="576"/>
+</tileset>
---
+++ data/tilesets/world.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="world" tilewidth="64" tileheight="64" tilecount="210" columns="10">
+ <image source="../../graphics/tilesets/world.png" width="640" height="1344"/>
+</tileset>
