d064dca2e228438cd7e8a4e1e07c70919a5ef265
Clear Code <62236986+clear-code-projects@users.noreply.github.com>
2024-04-14 15:52:07 +0100

    Add files via upload

---
+++ code (start)/game_data.py
@@ -0,0 +1,343 @@
+TRAINER_DATA = {
+	'o1': {
+		'monsters': {0: ('Jacana', 14), 1: ('Cleaf', 15)},
+		'dialog': {
+			'default': ['Hey, how are you?', 'Oh, so you want to fight?', 'FIGHT!'], 
+			'defeated': ['You are very strong!', 'Let\'s fight again sometime?']},
+		'directions': ['down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o2': {
+		'monsters': {0: ('Atrox', 14), 1: ('Pouch', 15), 2: ('Draem', 13), 3: ('Cindrill', 13)},
+		'dialog': {
+			'default': ['I don\'t like sand', 'It\'s coarse and rough', 'oh god, fight'], 
+			'defeated': ['May the force be with you']},
+		'directions': ['left', 'down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'o3': {
+		'monsters': {0: ('Atrox', 14), 1: ('Pouch', 15), 2: ('Draem', 13), 3: ('Cindrill', 13)},
+		'dialog': {
+			'default': ['I love skating!', 'FIGHT!'], 
+			'defeated': ['Good luck with the boss', 'It\'s so cold in here']},
+		'directions': ['left', 'right', 'up', 'down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'o4': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love skating!', 'FIGHT!'], 
+			'defeated': ['Good luck with the boss', 'It\'s so cold in here']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o5': {
+		'monsters': {0: ('Plumette', 20), 1: ('Ivieron', 22), 2: ('Atrox',24), 3: ('Pouch', 19)},
+		'dialog': {
+			'default': ['I love skating!', 'FIGHT!'], 
+			'defeated': ['Good luck with the boss', 'It\'s so cold in here']},
+		'directions': ['up', 'right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'o6': {
+		'monsters': {0: ('Finsta', 15), 1: ('Finsta', 15), 2: ('Finsta', 15)},
+		'dialog': {
+			'default': ['I love skating!', 'FIGHT!'], 
+			'defeated': ['Good luck with the boss', 'It\'s so cold in here']},
+		'directions': ['down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'o7': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['There are no bugs in the snow!'], 
+			'defeated': ['Maybe I should check a vulcano...', 'It\'s so cold in here']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'p1': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p2': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p3': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'p4': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'px': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Atrox',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['I love trees', 'and fights'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'forest'
+		},
+	'w1': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w2': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w3': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w4': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'w5': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['It\'s so cold in here', 'maybe a fight will warm me up'], 
+			'defeated': ['Good luck with the boss!']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'wx': {
+		'monsters': {0: ('Friolera', 25), 1: ('Gulfin', 20), 2: ('Draem',24), 3: ('Finiette', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['down'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'ice'
+		},
+	'f1': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f2': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['right', 'left'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f3': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['right', 'left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f4': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['up', 'right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f5': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['left'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'f6': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['This place feels kinda warm...', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['right'],
+		'look_around': True,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'fx': {
+		'monsters': {0: ('Cindrill', 15), 1: ('Jacana', 20), 2: ('Draem',24), 3: ('Atrox', 30)},
+		'dialog': {
+			'default': ['Time to bring the heat', 'fight!'], 
+			'defeated': ['Congratultion!']},
+		'directions': ['down'],
+		'look_around': False,
+		'defeated': False,
+		'biome': 'sand'
+		},
+	'Nurse': {
+		'direction': 'down',
+		'radius': 0,
+		'look_around': False,
+		'dialog': {
+			'default': ['Welcome to the hospital', 'Your monsters have been healed'], 
+			'defeated': None},
+		'directions': ['down'],
+		'defeated': False,
+		'biome': None
+		}
+}
+
+MONSTER_DATA = {
+	'Plumette': {
+		'stats': {'element': 'plant', 'max_health': 15, 'max_energy': 17, 'attack': 4, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Ivieron', 15)},
+	'Ivieron': {
+		'stats': {'element': 'plant', 'max_health': 18, 'max_energy': 20, 'attack': 5, 'defense': 10, 'recovery': 6, 'speed': 1.2},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Pluma', 32)},
+	'Pluma': {
+		'stats': {'element': 'plant', 'max_health': 23, 'max_energy': 25, 'attack': 6, 'defense': 12, 'recovery': 7, 'speed': 1.8},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Sparchu': {
+		'stats': {'element': 'fire', 'max_health': 15, 'max_energy': 17, 'attack': 3, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Cindrill', 15)},
+	'Cindrill': {
+		'stats': {'element': 'fire', 'max_health': 18, 'max_energy': 20, 'attack': 4, 'defense': 10, 'recovery': 6, 'speed': 1.2},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Charmadillo', 33)},
+	'Charmadillo': {
+		'stats': {'element': 'fire', 'max_health': 27, 'max_energy': 23, 'attack': 6, 'defense': 17, 'recovery': 7, 'speed': 1.5},
+		'abilities': {0: 'scratch', 5: 'fire', 10:'explosion', 12: 'battlecry', 20:'annihilate'},
+		'evolve': None},
+	'Finsta': {
+		'stats': {'element': 'water', 'max_health': 13, 'max_energy': 17, 'attack': 2, 'defense': 8, 'recovery': 5, 'speed': 1.8},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Gulfin', 34)},
+	'Gulfin': {
+		'stats': {'element': 'water', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 2},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Finiette', 32)},
+	'Finiette': {
+		'stats': {'element': 'water', 'max_health': 27, 'max_energy': 23, 'attack': 4, 'defense': 17, 'recovery': 7, 'speed': 2.5},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Atrox': {
+		'stats': {'element': 'fire', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 1.9},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Pouch': {
+		'stats': {'element': 'plant', 'max_health': 23, 'max_energy': 25, 'attack': 4, 'defense': 12, 'recovery': 7, 'speed': 1.5},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Draem': {
+		'stats': {'element': 'plant', 'max_health': 23, 'max_energy': 25, 'attack': 4, 'defense': 12, 'recovery': 7, 'speed': 1.4},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Larvea': {
+		'stats': {'element': 'plant', 'max_health': 15, 'max_energy': 17, 'attack': 1, 'defense': 8, 'recovery': 5, 'speed': 1},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': ('Cleaf', 4)},
+	'Cleaf': {
+		'stats': {'element': 'plant', 'max_health': 18, 'max_energy': 20, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 1.6},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Jacana': {
+		'stats': {'element': 'fire', 'max_health': 12, 'max_energy': 19, 'attack': 3, 'defense': 10, 'recovery': 6, 'speed': 2.6},
+		'abilities': {0: 'scratch', 5: 'spark'},
+		'evolve': None},
+	'Friolera': {
+		'stats': {'element': 'water', 'max_health': 27, 'max_energy': 23, 'attack': 4, 'defense': 17, 'recovery': 7, 'speed': 2},
+		'abilities': {0: 'scratch', 5: 'spark', 15: 'splash', 20: 'ice', 25: 'heal'},
+		'evolve': None},
+}
+
+ATTACK_DATA = {
+	'burn':       {'target': 'opponent', 'amount': 2,    'cost': 15, 'element': 'fire',   'animation': 'fire'},
+	'heal':       {'target': 'player',   'amount': -1.2, 'cost': 15, 'element': 'plant',  'animation': 'green'},
+	'battlecry':  {'target': 'player',   'amount': 1.4,  'cost': 20, 'element': 'normal', 'animation': 'green'},
+	'spark':      {'target': 'opponent', 'amount': 1.1,  'cost': 20, 'element': 'fire',   'animation': 'fire'},
+	'scratch':    {'target': 'opponent', 'amount': 1.2,  'cost': 20, 'element': 'normal', 'animation': 'scratch'},
+	'splash':     {'target': 'opponent', 'amount': 2,    'cost': 15, 'element': 'water',  'animation': 'splash'},
+	'fire':       {'target': 'opponent', 'amount': 2,    'cost': 15, 'element': 'fire',   'animation': 'fire'},
+	'explosion':  {'target': 'opponent', 'amount': 2,    'cost': 90, 'element': 'fire',   'animation': 'explosion'},
+	'annihilate': {'target': 'opponent', 'amount': 2,    'cost': 15, 'element': 'fire',   'animation': 'explosion'},
+	'ice':        {'target': 'opponent', 'amount': 2,    'cost': 15, 'element': 'water',  'animation': 'ice'},
+}
