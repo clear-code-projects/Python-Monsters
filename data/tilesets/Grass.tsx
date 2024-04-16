34a8b50178fce93839d128fda029dbf5f2c9d355
Clear Code <62236986+clear-code-projects@users.noreply.github.com>
2024-04-16 18:18:10 +0100

    Add files via upload

---
+++ data/tilesets/Grass.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Grass" tilewidth="64" tileheight="64" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="64" height="64" source="../../graphics/objects/grass.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="../../graphics/objects/grass_ice.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="../../graphics/objects/sand.png"/>
+ </tile>
+</tileset>
---
+++ data/tilesets/objects.tsx
@@ -0,0 +1,91 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="objects" tilewidth="448" tileheight="448" tilecount="29" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="320" height="320" source="../../graphics/objects/house_small_alt.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="192" source="../../graphics/objects/ice_tree.png"/>
+ </tile>
+ <tile id="2">
+  <image width="128" height="192" source="../../graphics/objects/palm.png"/>
+ </tile>
+ <tile id="3">
+  <image width="128" height="192" source="../../graphics/objects/palm_alt.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="128" source="../../graphics/objects/palm_small.png"/>
+ </tile>
+ <tile id="6">
+  <image width="64" height="192" source="../../graphics/objects/ruin_pillar.png"/>
+ </tile>
+ <tile id="7">
+  <image width="64" height="104" source="../../graphics/objects/ruin_pillar_broke.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="128" source="../../graphics/objects/ruin_pillar_broke_alt.png"/>
+ </tile>
+ <tile id="9">
+  <image width="128" height="192" source="../../graphics/objects/teal_tree.png"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="192" source="../../graphics/objects/teal_tree_bushy.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="128" source="../../graphics/objects/teal_tree_small.png"/>
+ </tile>
+ <tile id="12">
+  <image width="448" height="448" source="../../graphics/objects/arean_fire.png"/>
+ </tile>
+ <tile id="13">
+  <image width="448" height="448" source="../../graphics/objects/arena_plant.png"/>
+ </tile>
+ <tile id="14">
+  <image width="448" height="448" source="../../graphics/objects/arena_water.png"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="192" source="../../graphics/objects/green_tree.png"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="192" source="../../graphics/objects/green_tree_bushy.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="128" source="../../graphics/objects/green_tree_small.png"/>
+ </tile>
+ <tile id="18">
+  <image width="384" height="384" source="../../graphics/objects/hospital.png"/>
+ </tile>
+ <tile id="19">
+  <image width="448" height="448" source="../../graphics/objects/house_large.png"/>
+ </tile>
+ <tile id="20">
+  <image width="448" height="448" source="../../graphics/objects/house_large_alt.png"/>
+ </tile>
+ <tile id="21">
+  <image width="320" height="320" source="../../graphics/objects/house_small.png"/>
+ </tile>
+ <tile id="22">
+  <image width="64" height="128" source="../../graphics/objects/gate_pillar.png"/>
+ </tile>
+ <tile id="24">
+  <image width="232" height="80" source="../../graphics/objects/gate_top.png"/>
+ </tile>
+ <tile id="25">
+  <image width="60" height="60" source="../../graphics/objects/grassrock1.png"/>
+ </tile>
+ <tile id="26">
+  <image width="56" height="60" source="../../graphics/objects/grassrock2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="60" height="60" source="../../graphics/objects/icerock1.png"/>
+ </tile>
+ <tile id="28">
+  <image width="56" height="60" source="../../graphics/objects/icerock2.png"/>
+ </tile>
+ <tile id="29">
+  <image width="60" height="60" source="../../graphics/objects/sandrock1.png"/>
+ </tile>
+ <tile id="30">
+  <image width="56" height="60" source="../../graphics/objects/sandrock2.png"/>
+ </tile>
+</tileset>
